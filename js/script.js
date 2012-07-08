4fcaead67941cb5878f7064fe2305662541e290f
Sparkling Science <abhinit@sparklingscience.com>
2012-07-08 13:04:36 +0530

    Career swatchbook added

--- js/script.js
+++ js/script.js
@@ -91,4 +91,20 @@ $(document).ready(function(){
 		return v > 4 ? v : undef;
 	}
 	
+	/*if ($("#sb-container").length > 0){
+		alert("hi");
+		 $( '#sb-container' ).swatchbook( {
+					// number of degrees that is between each item
+					angleInc	: 15,
+					neighbor	: 15,
+					// if it should be closed by default
+					initclosed	: true,
+					// index of the element that when clicked, triggers the open/close function
+					// by default there is no such element
+					closeIdx	: 11
+			});
+	}*/
+
+
+	
 });
